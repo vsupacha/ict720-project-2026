2d488dfa7498c06ca6eca5b5799f3b95b4a646ca
Supachai Vorapojpisut <vsupacha@gmail.com>
2026-03-24 09:05:43 +0700

    Add Docker compose

--- docker/data_api/main.py
+++ docker/data_api/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+import os
+from pymongo import MongoClient
 
+# Import env variables
+db_name = os.getenv('DB_NAME', 'demo_db')
+ap_prefix = os.getenv('AP_PREFIX')
+db_uri = os.getenv('DB_URI', None)
+db_name = os.getenv('DB_NAME', None)
+
+mgc = MongoClient(db_uri)
+try:
+    print('Connecting MongoDB')
+    db = mgc.get_database(db_name)
+except Exception as err:
+    print(err)
+    
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get('/')
+def main():
+    return {"status": "OK"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/wifi_ap/{ap_id}")
+def get_ap(ap_id: int):
+    ap_ssid = ap_prefix + '{:02d}'
+    return {"AP": ap_ssid.format(ap_id)}
--- docker/data_hub/app.py
+++ docker/data_hub/app.py
@@ -1,24 +1,37 @@
 import paho.mqtt.client as mqtt
+import os
+import json
+from pymongo import MongoClient
+
+# Import env variables
+mqtt_broker = os.getenv('MQTT_BROKER', 'broker.emqx.io')
+data_topic = os.getenv('DATA_TOPIC', 'data_topic')
+db_uri = os.getenv('DB_URI', None)
+db_name = os.getenv('DB_NAME', None)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("taist/aiot/YOUR_TEAM/data")
+    # Subscribing in on_connect()
+    client.subscribe(data_topic)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    payload = json.loads(msg.payload)
+    print(msg.topic + ': ' + str(payload))
+
+mgc = MongoClient(db_uri)
+try:
+    print('Connecting MongoDB')
+    db = mgc.get_database(db_name)
+except Exception as err:
+    print(err)
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
+print('Connecting MQTT broker')
+mqttc.connect(mqtt_broker, 1883, 60)
 
-mqttc.connect("broker.emqx.io", 1883, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
+# Blocking call that processes network traffic, dispatches callbacks
 mqttc.loop_forever()
