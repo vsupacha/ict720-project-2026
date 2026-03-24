7c64976ba67f749b56353c0628951a23e4284e5d
Supachai Vorapojpisut <vsupacha@gmail.com>
2026-03-24 13:15:18 +0700

    Add db code

--- docker/data_api/main.py
+++ docker/data_api/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
 import os
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
 
 # Import env variables
-db_name = os.getenv('DB_NAME', 'demo_db')
 ap_prefix = os.getenv('AP_PREFIX')
 db_uri = os.getenv('DB_URI', None)
 db_name = os.getenv('DB_NAME', None)
+col_name = os.getenv('DB_COL', None)
 
 mgc = MongoClient(db_uri)
 try:
     print('Connecting MongoDB')
     db = mgc.get_database(db_name)
+    db_col = db[col_name]
 except Exception as err:
     print(err)
     
@@ -24,4 +25,13 @@ def main():
 @app.get("/wifi_ap/{ap_id}")
 def get_ap(ap_id: int):
     ap_ssid = ap_prefix + '{:02d}'
-    return {"AP": ap_ssid.format(ap_id)}
+    try:
+        #ans = db_col.find_one({"AP":ap_ssid.format(ap_id)}, sort=[("_id", DESCENDING)])
+        ans = db_col.find({"AP":ap_ssid.format(ap_id)})
+        resp = [doc for doc in ans]
+        print(resp)
+        for doc in resp:
+            doc.pop('_id')
+    except:
+        resp = {'STATUS': 'Too early, try later'}
+    return resp
--- docker/data_hub/app.py
+++ docker/data_hub/app.py
@@ -8,6 +8,8 @@ mqtt_broker = os.getenv('MQTT_BROKER', 'broker.emqx.io')
 data_topic = os.getenv('DATA_TOPIC', 'data_topic')
 db_uri = os.getenv('DB_URI', None)
 db_name = os.getenv('DB_NAME', None)
+col_name = os.getenv('DB_COL', None)
+print(db_uri, db_name, col_name)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
@@ -19,13 +21,22 @@ def on_connect(client, userdata, flags, reason_code, properties):
 def on_message(client, userdata, msg):
     payload = json.loads(msg.payload)
     print(msg.topic + ': ' + str(payload))
-
+    try:
+        db_col.insert_one(payload)
+    except:
+        print('Too early, try later')
+                          
 mgc = MongoClient(db_uri)
 try:
     print('Connecting MongoDB')
     db = mgc.get_database(db_name)
 except Exception as err:
     print(err)
+try:
+    db.create_collection(col_name)
+    db_col = db[col_name]
+except:
+    db_col = db[col_name]
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
